ad2037c3bb7905a06ac4d9193fad98a99deebbd4
Awais Amjed <awaisamjed171@gmail.com>
2022-02-15 18:52:02 +0500

    Updated config to include scss

--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,22 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
   devServer: {
     static: './dist',
   },
